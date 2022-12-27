37c24181f53800bddc23f92b8c9a36f70c5d7d80
ferryanggriawan <ferrya1997@gmail.com>
2022-12-27 12:06:28 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <Text class="text-h1" />
-  <Row justify="center" align="start">
-    <div class="col col-sm-1">asdasd</div>
-    <div class="col col-sm-1">
+  <Row align="start">
+    <Col cols="6">Embuh</Col>
+    <Col lg="1">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam hic placeat ut minus beatae totam accusantium pariatur! Voluptas,
       vel repellat!
-    </div>
-    <div class="col col-sm-1">asdasd</div>
-    <div class="col col-sm-1">asdasd</div>
-    <div class="col col-sm-1">asdasd</div>
-    <div class="col col-sm-1">asdasd</div>
-    <div class="col col-sm-1">asdasd</div>
-    <div class="col col-sm-1">asdasd</div>
-    <div class="col col-sm-1">asdasd</div>
-    <div class="col col-sm-1">asdasd</div>
-    <div class="col col-sm-1">asdasd</div>
-    <div class="col col-sm-1">asdasd</div>
-    <div class="col col-sm-1">asdasd</div>
-    <div class="col col-sm-1">asdasd</div>
+    </Col>
+    <Col lg="1">asdasd</Col>
+    <Col lg="1">asdasd</Col>
+    <Col lg="1">asdasd</Col>
+    <Col lg="1">asdasd</Col>
+    <Col lg="1">asdasd</Col>
+    <Col lg="1">asdasd</Col>
+    <Col lg="1">asdasd</Col>
+    <Col lg="1">asdasd</Col>
+    <Col lg="1">asdasd</Col>
+    <Col lg="1">asdasd</Col>
+    <Col lg="1">asdasd</Col>
+    <Col lg="1">asdasd</Col>
   </Row>
 </template>
