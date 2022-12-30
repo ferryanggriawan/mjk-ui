8f6ec3b915976746fb1437e2943d132b58f8a059
ferryanggriawan <ferrya1997@gmail.com>
2022-12-31 06:30:26 +0700

    spacing

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,16 @@
 <script setup lang="ts">
 import TextSection from "./section/Text.vue";
+import ButtonSection from "./section/Button.vue";
 </script>
 
 <template>
   <!-- Button -->
-  <section>
-    <Row>
-      <Col cols="12">
-        <div>Small</div>
-        <Btn size="small">Small</Btn>
-        <Btn size="small" type="fill">Small</Btn>
-        <Btn size="small" type="outline">Small</Btn>
-        <Btn size="small" type="text">Small</Btn>
-        <Btn size="small" type="underline">Small</Btn>
-      </Col>
-      <Col cols="12">
-        <div>Normal | Default</div>
-        <Btn type="fill">Normal</Btn>
-        <Btn type="outline">Normal</Btn>
-        <Btn type="text">Normal</Btn>
-        <Btn type="underline">Normal</Btn>
-      </Col>
-      <Col cols="12">
-        <div>Large</div>
-        <Btn size="large">Large</Btn>
-        <Btn size="large" type="outline">Large</Btn>
-        <Btn size="large" type="text">Large</Btn>
-        <Btn size="large" type="underline">Large</Btn>
-      </Col>
-      <Col cols="12">
-        <div>Block</div>
-        <Btn block size="small">Large</Btn>
-        <Btn block size="normal">Large</Btn>
-        <Btn block size="large">Large</Btn>
-      </Col>
-    </Row>
-  </section>
+  <ButtonSection />
+
+  <div class="divider-wrapper">
+    <div class="divider"></div>
+  </div>
+
   <!-- Text & Heading -->
   <TextSection />
 </template>
---
+++ src/section/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <Row>
+      <Col cols="12">
+        <div class="pb-3">Small</div>
+        <Btn class="mx-3" size="small">Small</Btn>
+        <Btn class="mx-3" size="small" type="fill">Small</Btn>
+        <Btn class="mx-3" size="small" type="outline">Small</Btn>
+        <Btn class="mx-3" size="small" type="text">Small</Btn>
+        <Btn class="mx-3" size="small" type="underline">Small</Btn>
+      </Col>
+      <Col cols="12">
+        <div class="pb-3">Normal | Default</div>
+        <Btn class="mx-3" type="fill">Normal</Btn>
+        <Btn class="mx-3" type="outline">Normal</Btn>
+        <Btn class="mx-3" type="text">Normal</Btn>
+        <Btn class="mx-3" type="underline">Normal</Btn>
+      </Col>
+      <Col cols="12">
+        <div class="pb-3">Large</div>
+        <Btn class="mx-3" size="large">Large</Btn>
+        <Btn class="mx-3" size="large" type="outline">Large</Btn>
+        <Btn class="mx-3" size="large" type="text">Large</Btn>
+        <Btn class="mx-3" size="large" type="underline">Large</Btn>
+      </Col>
+      <Col cols="12">
+        <div class="pb-3">Block</div>
+        <Btn class="mb-5" block size="small">Large</Btn>
+        <Btn class="mb-5" block size="normal">Large</Btn>
+        <Btn class="mb-5" block size="large">Large</Btn>
+      </Col>
+    </Row>
+  </section>
+</template>
