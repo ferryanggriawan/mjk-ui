8aacafb3e71ea56ad54b8216f7a2b4b0f25a1474
ferryanggriawan <ferrya1997@gmail.com>
2023-01-02 03:13:18 +0700

    fix flexbox

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,16 @@ import ButtonSection from "./section/Button.vue";
 </script>
 
 <template>
+  <section>
+    <div class="m-input">
+      <div></div>
+      <div>
+        <input type="text" />
+      </div>
+      <div></div>
+    </div>
+  </section>
+
   <!-- Button -->
   <ButtonSection />
 
